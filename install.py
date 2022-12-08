f58079f983554a6e897b1952c835d7513f72c18f
batatinha-espacial <103390202+batatinha-espacial@users.noreply.github.com>
2022-12-08 19:15:29 -0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+os.system("pip install typer[all] PyGithub")
+kkk = requests.get("https://raw.githubusercontent.com/batatinha-espacial/cleberpkg/main/main.py").text
+os.mkdir("/cleberpkg")
+with open("/cleberpkg/main.py", "wt") as file:
+  file.write(kkk)
+with open("/cleberpkg/installed.json", "wt") as file:
+  file.write("{\n\n}")
+print("Installed cleberpkg")
